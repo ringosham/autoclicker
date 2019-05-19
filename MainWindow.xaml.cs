02e3569797b83ca6d4f71eff036c308a47050229
Ringo Sham <ringosham@gmail.com>
2019-05-19 20:13:08 +0100

    Add new dependency

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -56,7 +56,7 @@ namespace Autoclicker {
             KeyValueConfigurationCollection configCollection = manager.AppSettings.Settings;
             configCollection.Clear();
             configCollection.Add("keybind", "146");
-            configCollection.Add("mean", "0.2");
+            configCollection.Add("mean", "0.25");
             configCollection.Add("sigma", "0.01");
             configCollection.Add("clickOption", "left");
             manager.Save(ConfigurationSaveMode.Full);
